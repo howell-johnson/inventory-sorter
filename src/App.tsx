2567a1d1773e77e5f0f1faf5ac125fab7ac59cab
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-07-13 11:42:30 -0500

    Add mobile form and toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { Header } from "./components/Header";
 import { ThemeProvider } from "./components/ThemeProvider";
+import { Toaster } from "./components/ui/toaster";
+import { Header } from "./components/Header";
+import { MobileForm } from "./components/MobileForm";
 
 export default function App() {
   return (
     <ThemeProvider defaultTheme="system" storageKey="theme">
       <div className="flex min-h-screen flex-col bg-background dark:text-white">
         <Header />
+        <MobileForm />
       </div>
+      <Toaster />
     </ThemeProvider>
   );
 }
---
+++ src/components/MobileForm.tsx
@@ -0,0 +1,74 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@radix-ui/react-label";
+import { Button } from "./ui/button";
+
+export function MobileForm() {
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const beds = formData.get("beds");
+    const baths = formData.get("baths");
+    const sqft = formData.get("sqft");
+    console.log({ beds, baths, sqft });
+  }
+  return (
+    <form onSubmit={handleSubmit} className="mt-2 border-b px-4 pb-4">
+      <div className="flex flex-wrap gap-4">
+        <div className="flex w-fit items-baseline gap-2">
+          <Label className="text-lg" htmlFor="beds">
+            Beds
+          </Label>
+          <Select name="beds" defaultValue="1">
+            <SelectTrigger className="p-2 pr-1">
+              <SelectValue placeholder="1" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="1">1</SelectItem>
+              <SelectItem value="2">2</SelectItem>
+              <SelectItem value="3">3</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex w-fit items-baseline gap-2">
+          <Label className="text-lg" htmlFor="baths">
+            Baths
+          </Label>
+          <Select name="baths" defaultValue="1">
+            <SelectTrigger className="p-2 pr-1">
+              <SelectValue placeholder="1" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="1">1</SelectItem>
+              <SelectItem value="2">2</SelectItem>
+              <SelectItem value="3">3</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex w-fit items-baseline gap-2">
+          <Label className="text-lg" htmlFor="sqft">
+            Sqft
+          </Label>
+          <Select name="sqft" defaultValue="800">
+            <SelectTrigger className="p-2 pr-1">
+              <SelectValue placeholder="800+" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="800">800+</SelectItem>
+              <SelectItem value="1000">1000+</SelectItem>
+              <SelectItem value="1200">1200+</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
+      <Button type="submit" className="mt-4">
+        Find homes
+      </Button>
+    </form>
+  );
+}
