76581d5e5b8cb82d3fc6042251f0e5a8a660cc42
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-07-13 09:46:21 -0500

    Setup shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { parserPlugin } from "./tools/parser";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/kaden-inventory/" : "/",
   plugins: [react(), parserPlugin()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
