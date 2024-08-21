52e61c7da0b40288dd053d13bd168114dee061eb
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-08-21 09:13:09 -0500

    Remove parser, in favor of manually managed JSON

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { parserPlugin } from "./tools/parser";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/inventory-sorter/" : "/",
-  plugins: [react(), parserPlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
